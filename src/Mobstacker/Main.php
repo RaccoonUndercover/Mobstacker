9a992f50834483721b7d201ff9d422991e27b0f6
RaccoonUndercover <46036793+RaccoonUndercover@users.noreply.github.com>
2019-01-29 17:27:45 -0800

    Update Main.php

--- src/Mobstacker/Main.php
+++ src/Mobstacker/Main.php
@@ -7,13 +7,13 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
     
-public function onLoad() : void{
+public function onLoad(){
     $this->getLogger()->info("Plugin is being enabled. Please wait..");
 }
-public function onEnable() : void{
+public function onEnable(){
     $this->getLogger()->info("Plugin has been enabled succesfully. Looking for errors. (If no errors found, then you should be fine.");
 }
-public function onDisable() : void{
+public function onDisable(){
     $this->getLogger()->info("Plugin has been disabled. Did the server stop?");
 }
 }
