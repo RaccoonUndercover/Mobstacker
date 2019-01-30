7732ae06effbba2d8480de1ce67fdf2302d962c3
RaccoonUndercover <46036793+RaccoonUndercover@users.noreply.github.com>
2019-01-29 17:33:14 -0800

    Remove onLoad() from Main

--- src/Mobstacker/Main.php
+++ src/Mobstacker/Main.php
@@ -7,9 +7,6 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
     
-public function onLoad(){
-    $this->getLogger()->info("Plugin is being enabled. Please wait..");
-}
 public function onEnable(){
     $this->getLogger()->info("Plugin has been enabled succesfully. Looking for errors. (If no errors found, then you should be fine.");
 }
